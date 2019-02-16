5a0e09c5c482c6c167bcc33abd9d36d2db1e9379
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@ugent.be>
2019-02-16 11:03:36 +0100

    Create android studio project
    Created basic project structure via android studio.
    Added Robolectric to the project's build.gradle file and adapted the
    example unit test to ensure it works.

---
+++ app/src/test/java/com/aurora/aurora/ExampleUnitTest.java
@@ -0,0 +1,23 @@
+package com.aurora.aurora;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example Unit test class with Robolectric
+ */
+@RunWith(RobolectricTestRunner.class)
+public class ExampleUnitTest {
+
+    /**
+     * This test should check if the result of an addition is correct
+     */
+    @Test
+    public void addition_should_be_correct() {
+        assertEquals(2 + 2, 4);
+    }
+}
