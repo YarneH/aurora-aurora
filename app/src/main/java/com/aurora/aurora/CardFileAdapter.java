0fd9b9443b482d30ff9899da655ec9884c4498fe
Stijn De Pauw <smidpauw.depauw@ugent.be>
2019-02-26 15:45:35 +0100

    Expand card

--- app/src/main/java/com/aurora/aurora/CardFileAdapter.java
+++ app/src/main/java/com/aurora/aurora/CardFileAdapter.java
@@ -3,11 +3,13 @@ package com.aurora.aurora;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
@@ -15,6 +17,10 @@ import android.widget.TextView;
  */
 public class CardFileAdapter extends RecyclerView.Adapter<CardFileAdapter.CardFileViewHolder> {
     private int mAmount = 100;
+    private int mSelectedIndex = -1;
+
+    private int mCardHeight;
+    private int mExpandHeight;
 
     public CardFileAdapter(){}
 
@@ -38,23 +44,57 @@ public class CardFileAdapter extends RecyclerView.Adapter<CardFileAdapter.CardFi
 
     public class CardFileViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView mTextView;
+        private CardView mCardView;
         private Button mButton;
+        private int index;
 
         public CardFileViewHolder(View itemView) {
             super(itemView);
+            mCardView = (CardView) itemView.findViewById(R.id.cv_file);
             mTextView = (TextView) itemView.findViewById(R.id.tv_card_title);
             mButton = (Button) itemView.findViewById(R.id.button_card_file);
+            mCardView.setOnClickListener(this);
             mButton.setOnClickListener(this);
         }
 
         public void bind(int i){
+            index = i;
             mTextView.setText("File " + String.valueOf(i));
+
+            if (i == mSelectedIndex) {
+                expand(mCardView);
+            } else {
+                collapse(mCardView);
+            }
         }
 
         @Override
         public void onClick(View view) {
-            Snackbar.make(view, "This will open " + mTextView.getText() + " with the previously selected plugin", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
+            if(view.getId() == R.id.cv_file) {
+                mSelectedIndex = index;
+                expand(view);
+            } else if(view.getId() == R.id.button_card_file) {
+                Snackbar.make(view, "This will open " + mTextView.getText() + " with the previously selected plugin", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
         }
+    }
+
+
+    public static void expand(final View v) {
+        v.measure(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        // Older versions of android (pre API 21) cancel animations for views with a height of 0.
+        CardView mCardView = (CardView) v;
+        RecyclerView mRecyclerView = (RecyclerView) mCardView.getParent();
+        final int targetHeight = mRecyclerView.getMeasuredHeight();
+        final int childHeight = mCardView.getMeasuredHeight();
+
+        mCardView.setMinimumHeight(targetHeight);
+    }
+
+    public static void collapse(final View v) {
+        CardView mCardView = (CardView) v;
+        final int initialHeight = v.getMeasuredHeight();
+
     }
 }
--- app/src/main/java/com/aurora/aurora/MainActivity.java
+++ app/src/main/java/com/aurora/aurora/MainActivity.java
@@ -2,6 +2,7 @@ package com.aurora.aurora;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -9,7 +10,6 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -187,5 +187,6 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
 
