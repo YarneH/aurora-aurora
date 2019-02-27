0e5007c6bd2a2161a009900958c6de48abb8b11f
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@ugent.be>
2019-02-27 22:32:28 +0100

    Add callback mechanism and change signature

--- app/src/main/java/com/aurora/kernel/AuroraCommunicator.java
+++ app/src/main/java/com/aurora/kernel/AuroraCommunicator.java
@@ -1,10 +1,17 @@
 package com.aurora.kernel;
 
+import android.app.Activity;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 
 import com.aurora.kernel.event.OpenFileWithPluginRequest;
 import com.aurora.kernel.event.PluginSettingsRequest;
 import com.aurora.kernel.event.ListPluginsRequest;
+import com.aurora.plugin.BasicPlugin;
+
+import java.util.List;
+
+import io.reactivex.Observable;
 
 /**
  * Communicator class that communicates to Aurora app environment
@@ -21,10 +28,12 @@ public class AuroraCommunicator extends Communicator {
      * @param pluginName the name of the plugin to open the file with
      * @param fileRef a reference to the file that needs to be opened
      */
-    public void openFileWithPlugin(String pluginName, String fileRef){
+    public Observable<Fragment> openFileWithPlugin(String pluginName, String fileRef){
         this.mBus.post(new OpenFileWithPluginRequest(pluginName, fileRef));
 
+        // TODO: Before implementing this, first do testing
         Log.d("AuroraCommunicator", "Callback not implemented yet!");
+        return null;
     }
 
     /**
@@ -32,19 +41,23 @@ public class AuroraCommunicator extends Communicator {
      *
      * @param pluginName the name of the plugin to get the settings for
      */
-    public void getSettingsOfPlugin(String pluginName) {
+    public Observable<Class<? extends Activity>> getSettingsOfPlugin(String pluginName) {
         this.mBus.post(new PluginSettingsRequest(pluginName));
 
+        // TODO: Before implementing this, first do testing
         Log.d("AuroraCommunicator", "Callback not implemented yet!");
+        return null;
     }
 
     /**
      * Gets a list of all the available plugins
      */
-    public void getListofPlugins() {
+    public Observable<List<BasicPlugin>> getListofPlugins() {
         this.mBus.post(new ListPluginsRequest());
 
+        // TODO: Before implementing this, first do testing
         Log.d("AuroraCommunicator", "Callback not implemented yet!");
+        return null;
     }
 
 
