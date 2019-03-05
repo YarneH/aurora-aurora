60c1f619cdd5c0bd4bc4c03e5813b54750776c54
Jonas Cuypers <cuypersjonas001@gmail.com>
2019-03-05 22:54:32 +0100

    Removed itext and added comment PDF extractor

--- app/src/main/java/com/aurora/internalservice/internalprocessor/TextExtractorPDF.java
+++ app/src/main/java/com/aurora/internalservice/internalprocessor/TextExtractorPDF.java
@@ -1,6 +1,11 @@
 package com.aurora.internalservice.internalprocessor;
 
 public class TextExtractorPDF implements TextExtractor {
+    /**
+     * TODO: This method will extract the text using iText
+     * @param fileRef a reference to where the file can be found
+     * @return the extracted text from the file on fileRef
+     */
     @Override
     public ExtractedText extract(String fileRef) {
         return new ExtractedText("",null);
