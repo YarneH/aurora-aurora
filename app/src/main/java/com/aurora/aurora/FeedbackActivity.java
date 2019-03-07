e8abfca6dc20f454dd5f01cb63bc25bc71388c77
Stijn De Pauw <smidpauw.depauw@ugent.be>
2019-03-07 10:25:14 +0100

    Http -> Https

--- app/src/main/java/com/aurora/aurora/FeedbackActivity.java
+++ app/src/main/java/com/aurora/aurora/FeedbackActivity.java
@@ -11,12 +11,13 @@ import android.widget.EditText;
 import org.json.JSONObject;
 
 import java.io.DataOutputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class FeedbackActivity extends AppCompatActivity {
     private static final int OK_RESPONSE_CODE = 200;
-    EditText mEditTextFeedback;
+    private EditText mEditTextFeedback;
 
     /**
      * Runs on startup of the activity, in this case on startup of the app
@@ -78,12 +79,12 @@ public class FeedbackActivity extends AppCompatActivity {
          */
         protected Boolean doInBackground(String... args) {
             boolean success = false;
-            HttpURLConnection conn = null;
+            HttpsURLConnection conn = null;
 
             try (AutoCloseable conc = conn::disconnect){
                 // Setup the connection for the Slack Webhook
                 URL url = new URL("https://hooks.slack.com/services/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");
-                conn = (HttpURLConnection) url.openConnection();
+                conn = (HttpsURLConnection) url.openConnection();
                 conn.setRequestMethod("POST");
                 conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
                 conn.setRequestProperty("Accept", "application/json");
