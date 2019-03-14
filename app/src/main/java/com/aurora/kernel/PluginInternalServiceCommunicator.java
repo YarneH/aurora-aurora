8c3a8fddc8b40388d57adf868bc59703c40a615b
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@ugent.be>
2019-03-14 13:32:06 +0100

    Change posting response

--- app/src/main/java/com/aurora/kernel/PluginInternalServiceCommunicator.java
+++ app/src/main/java/com/aurora/kernel/PluginInternalServiceCommunicator.java
@@ -38,6 +38,6 @@ public class PluginInternalServiceCommunicator extends Communicator {
         InternalProcessorResponse response = new InternalProcessorResponse(extractedText);
 
         // Post response
-        mBus.post(response); 
+        mBus.post(response);
     }
 }
