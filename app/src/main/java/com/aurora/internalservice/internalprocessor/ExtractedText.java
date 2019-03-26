6ab48d42f187685e1c6daef5350179472d08e13b
Jonas Cuypers <cuypersjonas001@gmail.com>
2019-03-26 23:25:59 +0100

    Create first implementation of txt extraction

--- app/src/main/java/com/aurora/internalservice/internalprocessor/ExtractedText.java
+++ app/src/main/java/com/aurora/internalservice/internalprocessor/ExtractedText.java
@@ -2,26 +2,68 @@ package com.aurora.internalservice.internalprocessor;
 
 import com.aurora.internalservice.InternallyProcessedFile;
 
+import java.util.Date;
 import java.util.List;
 
 /**
  * Class to represent extracted text from an internal processor
  */
 public class ExtractedText implements InternallyProcessedFile {
-    private String mTitle;
+    private String filename;
+    private Date dateLastEdit;
+    private String title;
+    private List<String> authors;
+    private List<Section> sections;
 
-    private List<String> mParagraphs;
 
-    public ExtractedText(String title, List<String> paragraphs) {
-        mTitle = title;
-        mParagraphs = paragraphs;
+    public ExtractedText() {
+    }
+
+    public ExtractedText(String filename, Date dateLastEdit, String title, List<String> authors, List<Section> sections) {
+        this.filename = filename;
+        this.dateLastEdit = dateLastEdit;
+        this.title = title;
+        this.authors = authors;
+        this.sections = sections;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public Date getDateLastEdit() {
+        return dateLastEdit;
+    }
+
+    public void setDateLastEdit(Date dateLastEdit) {
+        this.dateLastEdit = dateLastEdit;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public void setSections(List<Section> sections) {
+        this.sections = sections;
     }
 
     public String getTitle() {
-        return mTitle;
+        return title;
     }
 
-    public List<String> getParagraphs() {
-        return mParagraphs;
+    public List<Section> getSections() {
+        return sections;
     }
 }
--- app/src/main/java/com/aurora/internalservice/internalprocessor/TextExtractorTXT.java
+++ app/src/main/java/com/aurora/internalservice/internalprocessor/TextExtractorTXT.java
@@ -1,16 +1,63 @@
 package com.aurora.internalservice.internalprocessor;
 
+import android.util.Log;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class TextExtractorTXT implements TextExtractor {
 
     /**
      * Extracts the text from a .txt file.
+     *
      * @param fileRef a reference to where the file can be found
      * @return an ExtractedText object without title and one line per paragraph.
      */
     @Override
     public ExtractedText extract(InputStream file, String fileRef) {
-        return new ExtractedText(fileRef, null);
+        String content = extractStringFromTXT(file, fileRef);
+        String[] splitContent = content.split("\n");
+        List<Section> sections = new ArrayList<>();
+        String title = "";
+        if (splitContent.length > 3) {
+            title = splitContent[0];
+            for (int i = 1; i < splitContent.length; i++) {
+                sections.add(new Section("", splitContent[i], null));
+            }
+        }
+        return new ExtractedText(fileRef, Calendar.getInstance().getTime(), title, new ArrayList<>(), sections);
+    }
+
+    /**
+     * This method reads a txt file and puts all the content in a string
+     *
+     * @param inputStream this stream will be used to read the text
+     * @param fileRef     the filename for error handling
+     * @return the extracted plain text
+     */
+    private String extractStringFromTXT(InputStream inputStream, String fileRef) {
+        try {
+            // Use this for reading the data.
+            byte[] buffer = new byte[1000];
+            // read fills buffer with data and returns
+            StringBuilder outcome = new StringBuilder();
+            while (inputStream.read(buffer) != -1) {
+                outcome.append(new String(buffer));
+            }
+
+            inputStream.close();
+            Log.d("TXT", outcome.toString());
+            return outcome.toString();
+        } catch (FileNotFoundException ex) {
+            Log.e("Unable to open file", "Unable to open file '" + fileRef + "'" + ex.getLocalizedMessage());
+        } catch (IOException ex) {
+            Log.e("Error reading file", "Error reading file'" + fileRef + "' " + ex.getLocalizedMessage());
+        }
+        return null;
+
     }
 }
