c66d6ab6d8c8dde15e303b3597ffbcc541197c93
yarnehermann <yarne.hermann@ugent.be>
2019-03-27 04:04:03 +0100

    Fix Sonarqube issues again again

--- app/src/main/java/com/aurora/aurora/CardFileAdapter.java
+++ app/src/main/java/com/aurora/aurora/CardFileAdapter.java
@@ -157,7 +157,7 @@ public class CardFileAdapter extends RecyclerView.Adapter<CardFileAdapter.CardFi
                     RecyclerView recyclerView = (RecyclerView) view.getParent();
                     CardFileViewHolder prev = (CardFileViewHolder) recyclerView.findViewHolderForLayoutPosition(
                             mSelectedIndex);
-                    if (prev != null) collapse(prev.mCardView);
+                    if (prev != null) { collapse(prev.mCardView); }
                     mSelectedIndex = index;
                     expand(view);
                 }
