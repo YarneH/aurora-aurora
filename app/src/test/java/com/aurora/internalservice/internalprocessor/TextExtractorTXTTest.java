f156df2410bd78f710e75da35159710e38867f04
Jonas Cuypers <cuypersjonas001@gmail.com>
2019-04-09 21:41:45 +0200

    textextractorTXT tests cleanup

--- app/src/test/java/com/aurora/internalservice/internalprocessor/TextExtractorTXTTest.java
+++ app/src/test/java/com/aurora/internalservice/internalprocessor/TextExtractorTXTTest.java
@@ -2,6 +2,7 @@ package com.aurora.internalservice.internalprocessor;
 
 import com.aurora.auroralib.ExtractedText;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -12,27 +13,46 @@ import java.io.InputStream;
 import static org.junit.Assert.*;
 
 public class TextExtractorTXTTest {
-    private static TextExtractorTXT mTextExtractorTXT = new TextExtractorTXT();
+    private ExtractedText mExtractedText;
+
+    @Before
+    public void initialize() throws FileNotFoundException {
+        TextExtractorTXT mTextExtractorTXT = new TextExtractorTXT();
+        String mfileRef = "src/test/res/Pasta.txt";
+        File file = new File(mfileRef);
+        InputStream inputStream = new FileInputStream(file);
+        mExtractedText = mTextExtractorTXT.extract(inputStream, mfileRef);
+    }
+
 
     @Test
-    public void extract_shouldExtractTextCorrectly() {
-        String fileRef = "src/test/res/Pasta.txt";
-        File file = new File(fileRef);
-        try {
-            InputStream inputStream = new FileInputStream(file);
-            ExtractedText extractedText = mTextExtractorTXT.extract(inputStream, fileRef);
-            assertEquals("TXT text extraction:Titles are not the same",
-                    extractedText.getTitle(), "Pasta puttanesca");
-            assertEquals("TXT text extraction: Section is not extracted correctly",
-                    "Yield\n    4 servings\n" +
-                            "Active Time\n    30 minutes\n" +
-                            "Total Time\n    35 minutes",
-                    extractedText.getSections().get(0).getBody());
-            assertEquals("TXT text extraction: Section is not extracted correctly",
-                    "Ingredients",
-                    extractedText.getSections().get(1).getBody());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+    public void extract_shouldReturnExtractedText() {
+        assertNotNull("TXT text extraction: Failed to extract", mExtractedText);
     }
+
+
+    @Test
+    public void extract_shouldExtractTitleCorrectly(){
+        assertEquals("TXT text extraction: Titles are not the same",
+                "Pasta puttanesca",
+                mExtractedText.getTitle());
+    }
+
+    @Test
+    public void extract_shouldExtractSectionZeroCorrectly(){
+        assertEquals("TXT text extraction: Section is not extracted correctly",
+                "Yield\n    4 servings\n" +
+                        "Active Time\n    30 minutes\n" +
+                        "Total Time\n    35 minutes",
+                mExtractedText.getSections().get(0).getBody());
+    }
+
+    @Test
+    public void extract_shouldExtractSectionOneCorrectly(){
+        assertEquals("TXT text extraction: Section is not extracted correctly",
+                "Ingredients",
+                mExtractedText.getSections().get(1).getBody());
+    }
+
+
 }
