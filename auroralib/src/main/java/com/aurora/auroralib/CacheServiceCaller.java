0d141a1f6750e8ae6c8e8420d61b9838c23c859a
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@ugent.be>
2019-04-18 10:48:59 +0200

    Update labels for logs

--- auroralib/src/main/java/com/aurora/auroralib/CacheServiceCaller.java
+++ auroralib/src/main/java/com/aurora/auroralib/CacheServiceCaller.java
@@ -1,50 +1,45 @@
 package com.aurora.auroralib;
 
-import android.app.Application;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.pm.ServiceInfo;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
 import com.aurora.internalservice.internalcache.ICache;
 
 import java.util.List;
 
 public class CacheServiceCaller implements ServiceConnection {
+    private static final String LOG_TAG = "CACHE_SERVICE_CALLER";
+
     private ICache mBinding = null;
     //private ICache cacheBinding = null;
     // !!! Not sure yet if this is handled right by just passing an activity's context (See BasicPlugin_Old)
-    private Context mAppContext = null;
+    private Context mAppContext;
     //PackageManager pm = getPackageManager();
     private Object monitor = new Object();
 
-    public CacheServiceCaller(Context context){
+    public CacheServiceCaller(Context context) {
         mAppContext = context;
     }
 
 
     //TODO See how to integrate this properly (should be called form processor so skip activities
-    public void bindService(){
+    public void bindService() {
         Intent implicit = new Intent(ICache.class.getName());
         //Intent implicit = new Intent(IDownload.class.getName());
         List<ResolveInfo> matches = mAppContext.getPackageManager().queryIntentServices(implicit, 0);
         if (matches.size() == 0) {
-            Log.d("PLUGIN_CACHE_SERVICE", "No cache service found");
-        }
-        else if (matches.size() > 1) {
-            Log.d("PLUGIN_CACHE_SERVICE", "Multiple cache services found");
-        }
-        else {
-            Log.d("PLUGIN_CACHE_SERVICE", "1 cache services found");
+            Log.d(LOG_TAG, "No cache service found");
+        } else if (matches.size() > 1) {
+            Log.d(LOG_TAG, "Multiple cache services found");
+        } else {
+            Log.d(LOG_TAG, "1 cache service found");
             Intent explicit = new Intent(implicit);
             ServiceInfo svcInfo = matches.get(0).serviceInfo;
             ComponentName cn = new ComponentName(svcInfo.applicationInfo.packageName,
@@ -52,16 +47,16 @@ public class CacheServiceCaller implements ServiceConnection {
 
             explicit.setComponent(cn);
             mAppContext.bindService(explicit, this, Context.BIND_AUTO_CREATE);
-            Log.d("PLUGIN", "Binding");
+            Log.d(LOG_TAG, "Binding service");
         }
     }
 
-    public void unbindService(){
+    public void unbindService() {
         mAppContext.unbindService(this);
         disconnect();
     }
 
-    public int cache(String pluginObjectJSON){
+    public int cache(String pluginObjectJSON) {
         CacheThread cacheThread = new CacheThread(pluginObjectJSON);
         cacheThread.start();
         /*try {
@@ -83,7 +78,7 @@ public class CacheServiceCaller implements ServiceConnection {
     public void onServiceConnected(ComponentName className, IBinder binder) {
         synchronized (monitor) {
             mBinding = ICache.Stub.asInterface(binder);
-            Log.d("PLUGIN", "Bound");
+            Log.d(LOG_TAG, "Plugin Bound");
 
             monitor.notify();
         }
@@ -96,15 +91,15 @@ public class CacheServiceCaller implements ServiceConnection {
     }
 
     private void disconnect() {
-        mBinding=null;
-        Log.d("PLUGIN", "UnBound");
+        mBinding = null;
+        Log.d(LOG_TAG, "Plugin UnBound");
     }
 
     private class CacheThread extends Thread {
         private int mCacheResult = -1000;
         private String pluginObjectJSON;
 
-        protected CacheThread(String pluginObjectJSON){
+        protected CacheThread(String pluginObjectJSON) {
             this.pluginObjectJSON = pluginObjectJSON;
         }
 
@@ -112,8 +107,8 @@ public class CacheServiceCaller implements ServiceConnection {
             return mCacheResult;
         }
 
-        public void run(){
-            Log.d("PLUGIN", "cache called");
+        public void run() {
+            Log.d(LOG_TAG, "cache called");
             try {
                 /*
                 int countSleepIterations = 0;
@@ -127,17 +122,16 @@ public class CacheServiceCaller implements ServiceConnection {
                 }*/
 
 
-
                 if (mBinding == null) {
                     synchronized (monitor) {
-                        Log.d("PLUGIN", "Entering sync block" + mCacheResult);
+                        Log.d(LOG_TAG, "Entering sync block" + mCacheResult);
                         try {
                             monitor.wait();
                         } catch (InterruptedException e) {
                             Log.e(getClass().getSimpleName(), "Exception requesting cache", e);
                         }
                         mCacheResult = mBinding.cache(pluginObjectJSON);
-                        Log.d("PLUGIN", "" + mCacheResult);
+                        Log.d(LOG_TAG, "" + mCacheResult);
                     }
 
                     //Log.e(getClass().getSimpleName(), "Number of sleep iterations exceeded");
@@ -145,12 +139,11 @@ public class CacheServiceCaller implements ServiceConnection {
                 // TODO remove
                 else {
                     mCacheResult = mBinding.cache(pluginObjectJSON);
-                    Log.d("PLUGIN", "" + mCacheResult);
+                    Log.d(LOG_TAG, "" + mCacheResult);
 
                 }
 
-            }
-            catch (RemoteException e) {
+            } catch (RemoteException e) {
                 Log.e(getClass().getSimpleName(), "Exception requesting cache", e);
                 mCacheResult = -1;
             }
