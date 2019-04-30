4e4166214ec1bfd679a72763a3e9ec695e319697
lrenders <luca.renders@ugent.be>
2019-04-30 17:27:02 +0200

    Fix unit tests

--- app/src/test/java/com/aurora/internalservice/internaltranslation/TranslatorUnitTest.java
+++ app/src/test/java/com/aurora/internalservice/internaltranslation/TranslatorUnitTest.java
@@ -12,7 +12,6 @@ public class TranslatorUnitTest {
     @Test
     public void Translator_makeUrl_CorrectURLIsMade() throws UnsupportedEncodingException {
         String[] trans = {"this is a test", "what a cool test", "with escape characters / + . ", "how does that translate"};
-        Translator translator = new Translator();
         String response = Translator.makeUrl(trans, "en", "nl");
         String answer = "https://translation.googleapis.com/language/translate/v2?q=this+is+a+test&q=what+a+cool+test&q=with+escape+characters+%2F+%2B+.+&q=how+does+that+translate&target=nl&source=en&key=AIzaSyBT0I7M_hZlTmpD1hqC8beY1ILzpWGSU4s";
         assertEquals("they are not equal", answer, response);
