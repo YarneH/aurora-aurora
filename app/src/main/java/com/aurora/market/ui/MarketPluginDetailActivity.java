da96eae42eeb93fbe3b9c8a124b51a80c0211160
jotack <jonas.tack@ugent.be>
2019-05-05 01:14:41 +0200

    Add download of plugins

--- app/src/main/java/com/aurora/market/ui/MarketPluginDetailActivity.java
+++ app/src/main/java/com/aurora/market/ui/MarketPluginDetailActivity.java
@@ -1,15 +1,36 @@
 package com.aurora.market.ui;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
 
 import com.aurora.aurora.R;
+import com.aurora.market.data.database.MarketPlugin;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * An activity representing a single MarketPlugin detail screen. This
@@ -18,6 +39,8 @@ import com.aurora.aurora.R;
  * in a {@link MarketPluginListActivity}.
  */
 public class MarketPluginDetailActivity extends AppCompatActivity {
+    private MarketPlugin mMarketPlugin = null;
+    private BroadcastReceiver mBroadcastReceiver = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +49,15 @@ public class MarketPluginDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
 
+        mBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                // Empty because stub
+            }
+        };
+        registerReceiver(mBroadcastReceiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+
+        Activity activity = this;
         // Setup the FAB for downloading the Plugin
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_download_plugin);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +65,15 @@ public class MarketPluginDetailActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "This will download the plugin", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                int test = 9999;
+
+                Boolean readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+                Boolean writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+
+                if (!readPermission || !writePermission) {
+                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, test);
+                }
+                new DownloadPluginTask().execute(mMarketPlugin.getDownloadLink());
             }
         });
 
@@ -59,6 +100,8 @@ public class MarketPluginDetailActivity extends AppCompatActivity {
                     .add(R.id.marketplugin_detail_container, fragment)
                     .commit();
         }
+
+        mMarketPlugin = (MarketPlugin) getIntent().getSerializableExtra(MarketPluginDetailFragment.ARG_MARKET_PLUGIN);
     }
 
     @Override
@@ -71,4 +114,71 @@ public class MarketPluginDetailActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    private class DownloadPluginTask extends AsyncTask<URL, Void, Void> {
+
+        // TODO: show loading screen
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(URL... urls) {
+            try {
+                // Create request for android download manager
+                android.net.Uri uri = Uri.parse(urls[0].toString());
+                DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                DownloadManager.Request request = new DownloadManager.Request(uri);
+                request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI |
+                        DownloadManager.Request.NETWORK_MOBILE);
+
+                // Set title and description
+                request.setTitle("souschef.apk");
+                request.setDescription("Downloading the new plugin!");
+
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
+
+                // Set the local destination for download file to a path within the application's external files directory
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "souschef.apk");
+                request.setMimeType("application/vnd.android.package-archive");
+
+                long downloadID = downloadManager.enqueue(request);
+
+                mBroadcastReceiver = new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        long id = intent.getIntExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+                        if (id == downloadID) {
+                            // TODO: This code is not accessed
+                            Log.d("test","test");
+                            Intent installIntent = new Intent(Intent.ACTION_VIEW);
+                            installIntent.setDataAndType(Uri.fromFile(new File(Environment.DIRECTORY_DOWNLOADS + "plugin.apk")), "application/vnd.android.package-archive");
+                            installIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(installIntent);
+                        }
+                    }
+                };
+
+                //Intent intent = new Intent(Intent.ACTION_VIEW);
+                //intent.setDataAndType(Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/download/" + "app.apk")), "application/vnd.android.package-archive");
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                //startActivity(intent);
+            } catch (Exception e) {
+                Log.e("Download", "exception", e);
+            }
+
+            return null;
+        }
+
+        // TODO: delete file and stop loading screen
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Log.d("Download", "finished");
+            super.onPostExecute(aVoid);
+        }
+    }
+
 }
