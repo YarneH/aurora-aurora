ed84bd649e4b73dd0e8f25b3e4decff9d6a96fc0
yarnehermann <yarne.hermann@ugent.be>
2019-05-09 04:09:15 +0200

    Fix Sonar 2

--- app/src/main/java/com/aurora/aurora/PluginAdapter.java
+++ app/src/main/java/com/aurora/aurora/PluginAdapter.java
@@ -2,7 +2,6 @@ package com.aurora.aurora;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +21,8 @@ public class PluginAdapter extends ArrayAdapter<Plugin>{
         private TextView pluginName;
         private TextView pluginDescription;
         private ImageView pluginLogo;
+
+        private ViewHolder(){}
     }
 
     public PluginAdapter(List<Plugin> plugins, Context context) {
@@ -43,7 +44,7 @@ public class PluginAdapter extends ArrayAdapter<Plugin>{
         // Get the data item for this position
         Plugin plugin = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
-        ViewHolder viewHolder; 
+        ViewHolder viewHolder;
 
         final View result;
 
