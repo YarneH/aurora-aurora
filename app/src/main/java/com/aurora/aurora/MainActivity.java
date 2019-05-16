5757654af3570e35905151230288adbbe733ccae
Stijn De Pauw <smidpauw.depauw@ugent.be>
2019-05-16 15:18:38 +0200

    Add better visuals

--- app/src/main/java/com/aurora/aurora/MainActivity.java
+++ app/src/main/java/com/aurora/aurora/MainActivity.java
@@ -19,6 +19,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -149,6 +150,7 @@ public class MainActivity extends AppCompatActivity
 
         /* Setup RecyclerView */
         mRecyclerView = findViewById(R.id.rv_files);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         CardFileAdapter adapter = new CardFileAdapter(mKernel, this, mCachedFileInfoList);
         mRecyclerView.setAdapter(adapter);
